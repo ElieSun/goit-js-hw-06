4572316fa54583bf79c4c656bb496304f04635c1
ElieSun <elinavoloshyk@gmail.com>
2022-10-27 12:55:24 +0300

    Update task-05.js
    more secure

--- js/task-05.js
+++ js/task-05.js
@@ -2,8 +2,9 @@ const input = document.querySelector('#name-input');
 const output = document.querySelector('#name-output');
 
 input.addEventListener("input", (event)=> {
-    if(event.currentTarget.value)
+    if(event.currentTarget.value) { 
         output.textContent = event.currentTarget.value;
-    else
+    } else {
         output.textContent = 'Anonymous';
+    }
 });
