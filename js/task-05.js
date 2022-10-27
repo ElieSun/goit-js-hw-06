1d4d300cbe15c153f39e9b202d94fc50b92b7959
ElieSun <elinavoloshyk@gmail.com>
2022-10-27 12:50:57 +0300

    task-05
    Fix bug after clean input

--- js/task-05.js
+++ js/task-05.js
@@ -2,5 +2,8 @@ const input = document.querySelector('#name-input');
 const output = document.querySelector('#name-output');
 
 input.addEventListener("input", (event)=> {
-    output.textContent = event.currentTarget.value;
+    if(event.currentTarget.value)
+        output.textContent = event.currentTarget.value;
+    else
+        output.textContent = 'Anonymous';
 });
